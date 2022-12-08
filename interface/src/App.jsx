d73bc43f27180a15354f1f33f93e7b42f266d82b
joao gabriel <joaogabriel7303@gmail.com>
2022-12-08 16:16:38 -0300

    edição no campo do email

--- interface/src/App.jsx
+++ interface/src/App.jsx
@@ -4,13 +4,15 @@ import './App.css'
 function App() {
   return (
     <>
-    <form action="/dados" className='formulario'>
-      <label htmlFor="nome" className='text-center'><strong>Digite seu nome:</strong></label><br />
-      <input type="text" id='nome' placeholder='nome' /><br /><br />
-      <label htmlFor="senha" className='text-center'><strong>Digite sua senha:</strong></label><br />
-      <input type="password" id='senha' placeholder='senha' /><br /><br />
-      <input type="submit" value="ENVIAR" id='btn'  />
-    </form>
+      <form action="/" className='formulario'>
+        <label htmlFor="nome" className='text-center'><strong>Digite seu usuario:</strong></label><br />
+        <input type="text" id='nome' placeholder='nome' /><br /><br />
+        <label htmlFor="nome" className='text-center'><strong>Digite seu email:</strong></label><br />
+        <input type="email" id='nome' placeholder='email' /><br /><br />
+        <label htmlFor="senha" className='text-center'><strong>Digite sua senha:</strong></label><br />
+        <input type="password" id='senha' placeholder='senha' /><br /><br />
+        <input type="submit" value="ENVIAR" id='btn' />
+      </form>
     </>
   )
 }
